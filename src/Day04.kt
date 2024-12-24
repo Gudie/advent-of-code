ccca2bb90c76a46c5efbeb72b7a8a8012c6acfbf
Gudie <guydierx@gmail.com>
2024-12-24 18:16:24 +0100

    add day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,21 +1,68 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val arr = input.map { it.toList() }
+        return arr.foldIndexed(0) { x, acc, row ->
+            row.foldIndexed(acc) { y, rowAcc, cell ->
+
+                rowAcc + if (cell != 'X') 0 else {
+                    var count = 0
+                    for (dx in -1..1) {
+                        for (dy in -1..1) {
+                            var valid = true
+                            for ((index, symbol) in arrayOf('M', 'A', 'S').withIndex()) {
+                                try {
+                                    valid = valid && (arr[x + dx * (index + 1)][y + dy * (index + 1)] == symbol)
+                                } catch (e: Exception) {
+                                    valid = false
+                                }
+                            }
+                            count = count + if (valid) 1 else 0
+                        }
+                    }
+                    count
+                }
+            }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val arr = input.map { it.toList() }
+        val result = arr.foldIndexed(0) { x, acc, row ->
+            row.foldIndexed(acc) { y, rowAcc, cell ->
+
+                rowAcc + if (cell != 'A') 0 else {
+                    var count = 0
+                    try {
+                        if (
+                            (arr[x-1][y-1]=='M' && arr[x+1][y+1]=='S' ||
+                            arr[x+1][y+1]=='M' && arr[x-1][y-1]=='S' )
+                            &&
+                            (arr[x-1][y+1]=='M' && arr[x+1][y-1]=='S' ||
+                            arr[x+1][y-1]=='M' && arr[x-1][y+1]=='S')
+                        ) {
+                            count++
+                        }
+                    } catch (e: Exception) {
+                    }
+                    count
+                }
+            }
+        }
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+// Test if implementation meets criteria from the description, like:
+//    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day03_test")
-    check(part1(testInput) == 1)
+// Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day04_test")
+    val testInput2 = readInput("Day04_test2")
+    check(part1(testInput) == 4)
+    check(part1(testInput2) == 18)
+    check(part2(testInput2) == 9)
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day03")
+// Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day04")
     part1(input).println()
     part2(input).println()
 }
